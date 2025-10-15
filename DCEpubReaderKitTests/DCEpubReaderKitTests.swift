c6bd3d570cbf2090db98d70b7d77f129d275ce4c
Josep Cerdá Penadés <josep@daapcoders.com>
2025-10-15 09:18:42 +0200

    Initial commit

---
+++ DCEpubReaderKitTests/DCEpubReaderKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  DCEpubReaderKitTests.swift
+//  DCEpubReaderKitTests
+//
+//  Created by Josep Cerdá Penadés on 15/10/25.
+//
+
+import Testing
+@testable import DCEpubReaderKit
+
+struct DCEpubReaderKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
