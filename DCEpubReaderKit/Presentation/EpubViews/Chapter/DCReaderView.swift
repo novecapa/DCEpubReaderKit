ede9cff0579f482e823789357cada81c88e22d4c
Josep Cerdá Penadés <josep@daapcoders.com>
2025-11-06 22:16:10 +0100

    WIP

--- DCEpubReaderKit/Presentation/EpubViews/Chapter/DCReaderView.swift
+++ DCEpubReaderKit/Presentation/EpubViews/Chapter/DCReaderView.swift
@@ -18,7 +18,6 @@ struct DCReaderView: View {
     }
 
     @Environment(\.dismiss) var dismiss
-
     @ObservedObject var viewModel: DCReaderViewModel
 
     init(viewModel: DCReaderViewModel) {
@@ -29,89 +28,99 @@ struct DCReaderView: View {
         ZStack {
             viewModel.backgroundColor
                 .ignoresSafeArea(edges: .all)
-            GeometryReader { proxy in
-                TabView(selection: $viewModel.currentSelection) {
-                    ForEach(0..<viewModel.bookSpines.count, id: \.self) { idx in
-                        Group {
-                            if let chapterURL = viewModel.chapterURL(for: idx) {
-                                VStack {
-                                    DCChapterWebViewBuilder().build(chapterURL: chapterURL,
-                                                                    readAccessURL: viewModel.opfDirectoryURL,
-                                                                    spineIndex: idx,
-                                                                    userPreferences: viewModel.userPreferences) { action in
-                                        // TODO: Refactor action to viewModel + interactor
-                                        switch action {
-                                        case .totalPageCount(let count, let spineIndex):
-                                            if spineIndex == viewModel.currentSelection {
-                                                viewModel.totalPages = count
-                                                if viewModel.currentPage > count {
-                                                    viewModel.currentPage = count
-                                                }
-                                            }
-                                        case .currentPage(index: let index,
-                                                          totalPages: let totalPages,
-                                                          spineIndex: let spineIndex):
-                                            if spineIndex == viewModel.currentSelection {
-                                                viewModel.totalPages = totalPages
-                                                viewModel.currentPage = index
-                                            }
-                                        case .canTouch(let enabled):
-                                            Task { @MainActor in
-                                                viewModel.canTouch = enabled
-                                            }
-                                        case .coordsFirstNodeOfPage(orientation: _,
-                                                                    spineIndex: let spineIndex,
-                                                                    coords: let coords):
-                                            if spineIndex == viewModel.currentSelection {
-                                                // TODO: - Save book position
-                                                print("chapterFile: \(chapterURL.lastPathComponent) coords: \(coords)")
-                                            }
-                                        case .navigateToNextChapter:
-                                            if viewModel.currentSelection + 1 < viewModel.bookSpines.count {
-                                                viewModel.currentSelection += 1
-                                            }
-                                        case .navigateToPreviousChapter:
-                                            if viewModel.currentSelection > 0 {
-                                                viewModel.currentSelection -= 1
+            TabView(selection: $viewModel.currentSelection) {
+                ForEach(0..<viewModel.bookSpines.count, id: \.self) { idx in
+                    Group {
+                        if let chapterURL = viewModel.chapterURL(for: idx) {
+                            VStack {
+                                DCChapterWebViewBuilder().build(chapterURL: chapterURL,
+                                                                readAccessURL: viewModel.opfDirectoryURL,
+                                                                spineIndex: idx,
+                                                                userPreferences: viewModel.userPreferences) { action in
+                                    // TODO: Refactor action to viewModel + interactor
+                                    switch action {
+                                    case .totalPageCount(let count, let spineIndex):
+                                        if spineIndex == viewModel.currentSelection {
+                                            viewModel.totalPages = count
+                                            if viewModel.currentPage > count {
+                                                viewModel.currentPage = count
                                             }
                                         }
-                                    }.padding(.horizontal, 24)
-                                        .padding(.vertical, 12)
-                                        .id(viewModel.readerConfigId(for: idx))
-                                    if viewModel.totalPages > 1 {
-                                        Text(viewModel.pageInfo)
-                                            .font(.system(size: 14))
-                                            .foregroundStyle(viewModel.textColor)
-                                            .opacity(viewModel.canTouch ? 1 : 0)
-                                            .animation(.easeInOut(duration: 0.25),
-                                                       value: viewModel.canTouch)
+                                    case .currentPage(index: let index,
+                                                      totalPages: let totalPages,
+                                                      spineIndex: let spineIndex):
+                                        if spineIndex == viewModel.currentSelection {
+                                            viewModel.totalPages = totalPages
+                                            viewModel.currentPage = index
+                                        }
+                                    case .canTouch(let enabled):
+                                        Task { @MainActor in
+                                            viewModel.canTouch = enabled
+                                        }
+                                    case .coordsFirstNodeOfPage(orientation: _,
+                                                                spineIndex: let spineIndex,
+                                                                coords: let coords):
+                                        if spineIndex == viewModel.currentSelection {
+                                            // TODO: - Save book position
+                                            print("chapterFile: \(chapterURL.lastPathComponent) coords: \(coords)")
+                                        }
+                                    case .navigateToNextChapter:
+                                        if viewModel.currentSelection + 1 < viewModel.bookSpines.count {
+                                            viewModel.currentSelection += 1
+                                            defer {
+                                                viewModel.previousSelection = viewModel.currentSelection
+                                            }
+                                            NotificationCenter.default.post(
+                                                name: .chapterShouldScrollToLastPage,
+                                                object: nil,
+                                                userInfo: viewModel.currentSelection < viewModel.previousSelection ?
+                                                ["spineIndex": viewModel.currentSelection] :
+                                                    nil
+                                            )
+                                        }
+                                    case .navigateToPreviousChapter:
+                                        if viewModel.currentSelection > 0 {
+                                            viewModel.currentSelection -= 1
+                                            defer {
+                                                viewModel.previousSelection = viewModel.currentSelection
+                                            }
+                                            NotificationCenter.default.post(
+                                                name: .chapterShouldScrollToLastPage,
+                                                object: nil,
+                                                userInfo: viewModel.currentSelection < viewModel.previousSelection ?
+                                                ["spineIndex": viewModel.currentSelection] :
+                                                    nil
+                                            )
+                                        }
                                     }
+                                }.padding(.horizontal, 24)
+                                    .padding(.vertical, 12)
+                                    .id(viewModel.readerConfigId(for: idx))
+                                if viewModel.totalPages > 1 {
+                                    Text(viewModel.pageInfo)
+                                        .font(.system(size: 14))
+                                        .foregroundStyle(viewModel.textColor)
+                                        .opacity(viewModel.canTouch ? 1 : 0)
+                                        .animation(.easeInOut(duration: 0.25),
+                                                   value: viewModel.canTouch)
                                 }
-
-                            } else {
-                                Text("Unable to resolve chapter at spine index \(idx).")
-                                    .foregroundStyle(viewModel.textColor)
-                                    .padding()
                             }
+                        } else {
+                            Text("Unable to resolve chapter at spine index \(idx).")
+                                .foregroundStyle(viewModel.textColor)
+                                .padding()
                         }
-//                        .frame(width: proxy.size.width, height: proxy.size.height)
-//                                        .rotationEffect(.degrees(-90))
-//                                        .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0)) // flippingAngle
-                        .tag(idx)
                     }
+                    .tag(idx)
                 }
-//                .frame(width: proxy.size.height, height: proxy.size.width)
-//                            .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0)) // flippingAngle
-//                            .rotationEffect(.degrees(90), anchor: .topLeading)
-//                            .offset(x: proxy.size.width)
-                .tabViewStyle(.page(indexDisplayMode: .never))
-                .navigationBarBackButtonHidden(true)
-                .toolbar {
-                    toolbarView
-                }
-                .sheet(isPresented: $viewModel.showSettings) {
-                    sheetSettingsView
-                }
+            }
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                toolbarView
+            }
+            .sheet(isPresented: $viewModel.showSettings) {
+                sheetSettingsView
             }
         }
         .onChange(of: viewModel.currentSelection) { _ in
