6e327bf88cd6feef11e3c5d5c006ab0e4b0bbbc5
Josep Cerdá Penadés <josep@daapcoders.com>
2025-12-21 23:21:30 +0100

    WIP database

---
+++ DCEpubReaderKit/Database/Models/RBookMark.swift
@@ -0,0 +1,42 @@
+//
+//  RBookMark.swift
+//  DCEpubReaderKit
+//
+//  Created by Josep Cerdá Penadés on 21/12/25.
+//
+
+import Foundation
+import RealmSwift
+
+final class RBookMark: Object {
+
+    enum MarkType: String {
+        case hightLihgt
+        case note
+        case bookMark
+        case lastPosition
+    }
+
+    private enum Constants {
+        static let primaryKey = "uuid"
+    }
+
+    @Persisted var uuid: String = ""
+    @Persisted var bookId: String = ""
+    @Persisted var text: String = ""
+    @Persisted var coords: String = ""
+    @Persisted var chapterId: String = ""
+    @Persisted var chapterTitle: String = ""
+    @Persisted var pageNumber: Int = 0
+    @Persisted var textNote: String = ""
+    @Persisted var type: String = ""
+    @Persisted var lastcoords: String = ""
+    @Persisted var lastchapterid: String = ""
+    @Persisted var dateCreated: Int = 0
+    @Persisted var dateUpdated: Int = 0
+    @Persisted var state: Bool = true
+
+    public override static func primaryKey() -> String {
+        return Constants.primaryKey
+    }
+}
